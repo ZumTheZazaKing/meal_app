f4a5ff9359033b29ebc5720dbf50fd77730f57c8
ZumTheZazaKing <zahidi85543@gmail.com>
2025-10-26 21:36:44 +0800

    added prep for bookmark

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,62 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { FaBookmark } from "react-icons/fa";
 
 const Header = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const isAuth = !!localStorage.getItem("auth_token");
 
-    const logout = () => {
-        const token = localStorage.getItem('auth_token')
-        axios.post(import.meta.env.VITE_BASE_API_URL+'/logout',{},{
-            headers:{
-                Authorization:`Bearer ${token}`,
-            }
-        })
-        .then(res => {
-            let data = res.data
-            if(data.status != 200)return console.error('Logout failed')
+  const logout = () => {
+    const token = localStorage.getItem("auth_token");
+    axios
+      .post(
+        import.meta.env.VITE_BASE_API_URL + "/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        let data = res.data;
+        if (data.status != 200) return console.error("Logout failed");
 
-            localStorage.removeItem('auth_token')
-            navigate('/login')
-        })
-    }
+        localStorage.removeItem("auth_token");
+        navigate("/login");
+      });
+  };
 
-    return (
-        <div className="bg-zinc-100 shadow p-3 flex justify-end">
-            <button onClick={logout} className="tracking-wide text-zinc-500 cursor-pointer">LOGOUT</button>
-        </div>
-    )
+  const login = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="bg-zinc-100 shadow p-3 flex justify-between">
+      <div>
+        {isAuth && (
+          <button className="cursor-pointer text-zinc-500">
+            <FaBookmark size={24} />
+          </button>
+        )}
+      </div>
+      {isAuth ? (
+        <button
+          onClick={logout}
+          className="tracking-wide text-zinc-500 cursor-pointer"
+        >
+          LOGOUT
+        </button>
+      ) : (
+        <button
+          onClick={login}
+          className="tracking-wide text-zinc-500 cursor-pointer"
+        >
+          LOGIN
+        </button>
+      )}
+    </div>
+  );
 };
 export default Header
--- src/components/MealList.jsx
+++ src/components/MealList.jsx
@@ -1,19 +1,24 @@
-const MealList = ({meals, categories}) => {
-    return (
-      <div className="flex flex-col gap-3 ">
-        {meals &&
-          meals.map((meal, i) => {
-            return (
-              <div
-                className="rounded-[10px] shadow-lg flex gap-3 items-center"
-                key={i}
-              >
-                <img
-                  className="rounded-l-[10px]"
-                  src={meal.strMealThumb}
-                  width={150}
-                  alt=""
-                />
+import { FaRegBookmark } from "react-icons/fa";
+
+const MealList = ({ meals, categories }) => {
+  const isAuth = !!localStorage.getItem("auth_token");
+
+  return (
+    <div className="flex flex-col gap-3 ">
+      {meals &&
+        meals.map((meal, i) => {
+          return (
+            <div
+              className="rounded-[10px] shadow-lg flex gap-3 items-center"
+              key={i}
+            >
+              <img
+                className="rounded-l-[10px]"
+                src={meal.strMealThumb}
+                width={150}
+                alt=""
+              />
+              <div className="flex items-center justify-between w-full">
                 <div className="flex flex-col gap-1">
                   <div className="text-lg">
                     <b>{meal.strMeal}</b>
@@ -25,10 +30,16 @@ const MealList = ({meals, categories}) => {
                     {meal.strCategory}
                   </div>
                 </div>
+                {isAuth && (
+                  <button className="p-4 text-zinc-500 cursor-pointer">
+                    <FaRegBookmark size={24} />
+                  </button>
+                )}
               </div>
-            );
-          })}
-      </div>
-    );
+            </div>
+          );
+        })}
+    </div>
+  );
 };
-export default MealList
+export default MealList;
